0d22563958b67dd7abe7313c1a8c79ae33376dd5
LeDuy <leduy211194@gmail.com>
2017-02-21 13:51:32 +0700

    Add folder 'duy_frontend'
    To test link file css, js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title>VietMarketPlace</title>
-	<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/>
-	<link rel="stylesheet" href="{{asset('css/homepage.css')}}"/>
-	<script src="{{asset('js/jquery.min.js')}}"></script>
-	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+	<link rel="stylesheet" href="{{asset('duy_frontend/css/bootstrap.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('duy_frontend/css/homepage.css')}}"/>
+	<script src="{{asset('duy_frontend/js/jquery.min.js')}}"></script>
+	<script src="{{asset('duy_frontend/js/bootstrap.min.js')}}"></script>
 </head>
 <body>
 	@include('layouts.header')
